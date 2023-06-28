440553629959611db9c71263268606482a85b732
Akshay Kadam <akshaykadam0904@gmail.com>
2023-06-28 16:34:12 +0530

    fix cookies type bug by passing request null

--- src/lib/validate-session.ts
+++ src/lib/validate-session.ts
@@ -4,7 +4,7 @@ import { redirect } from 'next/navigation'
 import { auth } from '@/app/auth/lucia'
 
 export const validateSession = async (url: string, sessionExists: boolean) => {
-  const authRequest = auth.handleRequest({ cookies })
+  const authRequest = auth.handleRequest({ cookies, request: null })
   const session = await authRequest.validate()
 
   if (sessionExists) {
